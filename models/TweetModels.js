b12b55f12e353cce70e6f1f941e79106d256e590
halfrussian <84690420+halfrussian@users.noreply.github.com>
2025-01-06 10:59:22 -0600

    Update TweetModels.js

--- models/TweetModels.js
+++ models/TweetModels.js
@@ -1,19 +1,5 @@
-// const mongoose = require('mongoose');
-
-// const tweetSchema = new mongoose.Schema({
-//   tweetId: { type: String, required: true }, // Tweet ID
-//   username: { type: String, required: true }, // Username of the author
-//   text: { type: String, required: true }, // Tweet content
-//   media: [{ type: String }], // Array of media URLs (optional)
-// });
-
-// const Tweet = mongoose.model('Tweet', tweetSchema);
-
-// module.exports = Tweet;
-
 
 const mongoose = require('mongoose');
-
 const tweetSchema = new mongoose.Schema({
   tweetId: { type: String, required: false, unique: true }, // Ensure `tweetId` is unique
   username: { type: String, required: false },
